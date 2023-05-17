126b58d78ab3dde58e6392c219bfdf3f0402c7c7
GioPan04 <gioele@pannetto.com>
2023-05-17 18:57:10 +0200

    Added logos

--- src/header.php
+++ src/header.php
@@ -13,14 +13,14 @@ $menu_id = get_nav_menu_locations()['ortointelligente-navbar'];
 
 <nav class="navbar">
     <div class="container navbar-head">
-        <div className="flex items-center divide-x">
-            <a href="https://gobettivolta.edu.it/" target="_blank" className="">
-                <img src="/logo-gv.png" alt="Logo Gobetti Volta" className="w-16 object-contain" />
+        <div class="navbar-logo">
+            <a href="https://gobettivolta.edu.it/" target="_blank">
+                <img src="<?php echo get_template_directory_uri() ?>/imgs/logo-gv.png" alt="Logo Gobetti Volta"/>
             </a>
             <span className="ml-4 pl-4 text-2xl font-bold leading-6"><span className="text-primary">Ort</span><span className="text-[#b959a3]">o</span><br /><span className="text-[#f27931]">Intelligente</span></span>
         </div>
         <a href="https://www.sapereconsumare.it/" target="_blank">
-            <img className="w-32" src="/Logo-Sapere-Consumare.png" alt="Logo Sapere e Consumare" />
+            <img class="navbar-sc" src="<?php echo get_template_directory_uri() ?>/imgs/sapereconsumare.png" alt="Logo Sapere e Consumare" />
         </a>
     </div>
     <div class="navbar-content">
