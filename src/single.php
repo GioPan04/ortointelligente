18be03023cad4307230bddd4b0abdbac3a65d6b2
GioPan04 <gioele@pannetto.com>
2023-05-31 17:55:10 +0200

    Post page

--- src/single.php
+++ src/single.php
@@ -1,7 +1,10 @@
 <?php get_header(); ?>
 <div class="post-hero" style="background-image: url('<?php the_post_thumbnail_url(); ?>');">
-    <h1><?php the_title(); ?></h1>
+    <div class="post-hero-overlay">
+        <h1><?php the_title(); ?></h1>
+    </div>
 </div>
 <div class="container post-container">
+    <?php the_content(); ?>
 </div>
 <?php get_footer(); ?>
